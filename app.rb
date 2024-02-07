cd6dce4e65f366084023e3cefff89be73c493c68
Tim <tim@Tim.local>
2024-02-08 00:10:14 +0600

    added file new.erb

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require 'sinatra'
 require 'movie'
 
 get('/movies') do
-  @movie = Movie.new
-  @movie.title = 'Avengers'
+  @movies = []
+  @movies[0] = Movie.new
+  @movies[0].title = 'Avengers'
+  @movies[1] = Movie.new
+  @movies[1].title = 'Ant-Man'
+  @movies[2] = Movie.new
+  @movies[2].title = 'Spider-Man'
   erb :index
 end
 
+get('/movies/new') do
+  erb :new
+end
